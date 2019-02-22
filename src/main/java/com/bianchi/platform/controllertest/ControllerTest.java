84b8f7f7ae63199defc8c535c94c795ef2cf1dae
Alessandro Bianchi <alex24br@yahoo.it>
2019-02-22 18:14:45 +0100

    test

--- src/main/java/com/bianchi/platform/controllertest/ControllerTest.java
+++ src/main/java/com/bianchi/platform/controllertest/ControllerTest.java
@@ -1,16 +1,18 @@
-package com.bianchi.platform;
+package com.bianchi.platform.controllertest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ControllerTest
 {
-    @RequestMapping(value="/person", method= RequestMethod.GET)
+    @GetMapping("/")
     public String get()
     {
-        System.out.println("ciao");
-        return "troppo fico";
+        System.out.println("test ok");
+        return "index";
     }
 }
